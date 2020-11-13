8d79851a6c1e84a89ca058fa49d90457473e8152
Korobas2k20 <62026527+Korobas2k20@users.noreply.github.com>
2020-11-13 20:12:52 +0500

    Add files via upload

---
+++ urok7/js/mu.js
@@ -0,0 +1,7 @@
+function rotate() {
+    document.getElementById("rotate").style.transform = "rotateY(180deg)";
+}
+
+function rotate2() {
+    document.getElementById("rotate").style.transform = "rotateY(360deg)";
+}
